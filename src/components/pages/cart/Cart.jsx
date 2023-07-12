d5dad59802dc4e9a2a55cebef87e9b31b70d0870
agustinharmann <agustinharmann@gmail.com>
2023-07-12 03:49:02 -0300

    agrego escritura en carrito

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,7 +1,8 @@
 const Cart = () => {
   return (
-    <div>
-      carrito
+    <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+      <h1>Carrito</h1>
+      <h2>Futuramente los productos en el carrito</h2>
     </div>
   );
 };
