b9d92aeebc3b1b919de2933f768572f41057af2b
agustinharmann <agustinharmann@gmail.com>
2023-07-26 04:02:06 -0300

    cree y configuré al context del carrito

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ListContainer } from './components/pages/List/ListContainer';
 import { ItemDetailContainer } from './components/pages/itemDetail/ItemDetailContainer';
 import { CartContainer } from './components/pages/cart/CartContainer';
+import { CartContextContainer } from './context/CartContextContainer';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavbarContainer />
-      <Routes>
-        <Route path="/" element={<ListContainer />} />
-        <Route path="/category/:categoryName" element={<ListContainer />} />
-        <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-        <Route path="/cart" element={<CartContainer />} />
+      <CartContextContainer>
+
+        <NavbarContainer />
+        <Routes>
+          <Route path="/" element={<ListContainer />} />
+          <Route path="/category/:categoryName" element={<ListContainer />} />
+          <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<CartContainer />} />
 
 
-        <Route path='*' element={<h1>404 - Not found</h1>} />
+          <Route path='*' element={<h1>404 - Not found</h1>} />
 
-      </Routes>
+        </Routes>
+      </CartContextContainer>
     </BrowserRouter>
   )
 }
