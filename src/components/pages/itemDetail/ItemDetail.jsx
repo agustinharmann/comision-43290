d280fe5a6119e1b796a1c41e0b6b6807fd4512aa
agustinharmann <agustinharmann@gmail.com>
2023-09-10 20:52:47 -0300

    tercera prueba deploy con skeletons

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -12,28 +12,28 @@ const ItemDetail = ({ product, agregarAlCarrito, cantidadEnCarrito }) => {
           <Box>
             <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
               <Typography variant="h4" gutterBottom>
-                {product.title}
+                {product?.title}
               </Typography>
               <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-                Category: {product.category}
+                Category: {product?.category}
               </Typography>
-              <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product.title} image={product.image} />
+              <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product?.title} image={product?.image} />
               <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-                Price: ${product.price}
+                Price: ${product?.price}
               </Typography>
               <Typography variant="body1" style={{ marginBottom: '16px', width: '100%', maxWidth: '600px' }}>
-                {product.description}
+                {product?.description}
               </Typography>
               {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
                 Ya tenés {cantidadEnCarrito} unidades en tu carrito.
               </Typography>}
               <Typography variant="body2" color="textSecondary">
-                <strong>Stock</strong>: {product.stock} units
+                <strong>Stock</strong>: {product?.stock} units
               </Typography>
             </Box>
             <CounterContainer
               cantidadEnCarrito={cantidadEnCarrito}
-              stock={product.stock}
+              stock={product?.stock}
               agregarAlCarrito={agregarAlCarrito}
             />
           </Box>
