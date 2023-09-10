8883f446972c6392ef16b5c541c7cc86e17bb594
agustinharmann <agustinharmann@gmail.com>
2023-09-10 20:44:20 -0300

    tercera prueba deploy con skeletons

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardMedia, Typography } from "@mui/material";
+import { Box, Card, CardMedia, Skeleton, Stack, Typography } from "@mui/material";
 import { CounterContainer } from '../../../components/common/counter/CounterContainer';
 
 const ItemDetail = ({ product, agregarAlCarrito, cantidadEnCarrito }) => {
@@ -8,47 +8,48 @@ const ItemDetail = ({ product, agregarAlCarrito, cantidadEnCarrito }) => {
       <Card sx={{
         boxShadow: 'none'
       }}>
-
-        <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
-
-          {product?.title ? <Typography variant="h4" gutterBottom>
-            {product.title}
-          </Typography> : <Skeleton variant="h3" sx={{ fontSize: "1rem" }} />}
-
-          {product?.category ? <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-            Category: {product.category}
-          </Typography> : <Skeleton variant="text" sx={{ fontSize: "1rem" }} />}
-
-          {product?.image ? <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={(product?.title ? product.title : 'product image')} image={product.image} /> : <Skeleton variant="rectangular" width={210} height={60} />}
-
-          {product?.price ? <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-            Price: ${product.price}
-          </Typography> : <Skeleton variant="text" sx={{ fontSize: "1rem" }} />}
-
-          {product?.description ? <Typography variant="body1" style={{ marginBottom: '16px', width: '100%', maxWidth: '600px' }}>
-            {product.description}
-          </Typography> : <Box sx={{ width: 300 }}>
+        {product ? (
+          <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
+            <Typography variant="h4" gutterBottom>
+              {product.title}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+              Category: {product.category}
+            </Typography>
+            <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product.title} image={product.image} />
+            <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
+              Price: ${product.price}
+            </Typography>
+            <Typography variant="body1" style={{ marginBottom: '16px', width: '100%', maxWidth: '600px' }}>
+              {product.description}
+            </Typography>
+            {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
+              Ya tenés {cantidadEnCarrito} unidades en tu carrito.
+            </Typography>}
+            <Typography variant="body2" color="textSecondary">
+              <strong>Stock</strong>: {product.stock} units
+            </Typography>
+          </Box>
+        ) : <Stack style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
+          <Skeleton variant="h3" sx={{ fontSize: "1rem" }} />
+          <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+          <Skeleton variant="rectangular" width={210} height={60} />
+          <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+          <Box sx={{ width: 300 }}>
             <Skeleton />
             <Skeleton animation="wave" />
             <Skeleton animation={false} />
-          </Box>}
+          </Box>
+          <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+          <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+        </Stack>}
+      </Card>
 
-          {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
-            Ya tenés {cantidadEnCarrito} unidades en tu carrito.
-          </Typography>}
-
-          {product?.stock ? <Typography variant="body2" color="textSecondary">
-            <strong>Stock</strong>: {product.stock} units
-          </Typography> : <Skeleton variant="text" sx={{ fontSize: "1rem" }} />}
-        </Box>
-
-        <CounterContainer
-          cantidadEnCarrito={cantidadEnCarrito}
-          stock={product.stock}
-          agregarAlCarrito={agregarAlCarrito}
-        />
-
-      </Card >
+      <CounterContainer
+        cantidadEnCarrito={cantidadEnCarrito}
+        stock={product.stock}
+        agregarAlCarrito={agregarAlCarrito}
+      />
     </>
   );
 };
