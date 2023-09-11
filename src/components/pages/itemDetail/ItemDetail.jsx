a70a7095e3ae22f065971f0b1a620dff47f0a3c4
agustinharmann <agustinharmann@gmail.com>
2023-09-10 21:02:39 -0300

    tercera prueba deploy con skeletons

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -11,23 +11,23 @@ const ItemDetail = ({ product, agregarAlCarrito, cantidadEnCarrito }) => {
         <Box>
           <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
             <Typography variant="h4" gutterBottom>
-              { product.title ? product.title : <p>cargando...</p> }
+              {product.title ? product.title : <p>cargando...</p>}
             </Typography>
             <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-              {/* Category: {product?.category} */}
+              {product.category ? product.category : <p>cargando...</p>}
             </Typography>
             {/* <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product?.title} image={product?.image} /> */}
             <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-              {/* Price: ${product?.price} */}
+              {product.price ? product.price : <p>cargando...</p>}
             </Typography>
             <Typography variant="body1" style={{ marginBottom: '16px', width: '100%', maxWidth: '600px' }}>
-              {/* {product?.description} */}
+              {product.description ? product.description : <p>cargando...</p>}
             </Typography>
-            {/* {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
+            {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
               Ya tenés {cantidadEnCarrito} unidades en tu carrito.
-            </Typography>} */}
+            </Typography>}
             <Typography variant="body2" color="textSecondary">
-              {/* <strong>Stock</strong>: {product?.stock} units */}
+              {product.stock ? <p><strong>Stock</strong> : {product.stock} unidades </p> : <p>cargando...</p>}
             </Typography>
           </Box>
           <CounterContainer
