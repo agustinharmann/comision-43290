250b7cf6498927109c36ba275ed4943576075a5c
agustinharmann <agustinharmann@gmail.com>
2023-09-10 21:05:07 -0300

    tercera prueba deploy con skeletons

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -11,23 +11,24 @@ const ItemDetail = ({ product, agregarAlCarrito, cantidadEnCarrito }) => {
         <Box>
           <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '16px' }}>
             <Typography variant="h4" gutterBottom>
-              {product.title ? product.title : <p>cargando...</p>}
+              {product.title ? product.title : <p>Cargando...</p>}
             </Typography>
             <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-              {product.category ? product.category : <p>cargando...</p>}
+              {product.category ? product.category : <p>Cargando...</p>}
             </Typography>
-            {/* <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product?.title} image={product?.image} /> */}
+            {product.image ? <CardMedia style={{ width: 300, height: 300, marginBottom: '16px' }} component="img" alt={product?.title} image={product?.image} /> : <p>Cargando...</p>}
+            {/*  */}
             <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-              {product.price ? product.price : <p>cargando...</p>}
+              {product.price ? product.price : <p>Cargando...</p>}
             </Typography>
             <Typography variant="body1" style={{ marginBottom: '16px', width: '100%', maxWidth: '600px' }}>
-              {product.description ? product.description : <p>cargando...</p>}
+              {product.description ? product.description : <p>Cargando...</p>}
             </Typography>
             {cantidadEnCarrito && <Typography variant="h5" color="textSecondary">
               Ya tenés {cantidadEnCarrito} unidades en tu carrito.
             </Typography>}
             <Typography variant="body2" color="textSecondary">
-              {product.stock ? <p><strong>Stock</strong> : {product.stock} unidades </p> : <p>cargando...</p>}
+              {product.stock ? <p><strong>Stock</strong> : {product.stock} unidades </p> : <p>Cargando...</p>}
             </Typography>
           </Box>
           <CounterContainer
